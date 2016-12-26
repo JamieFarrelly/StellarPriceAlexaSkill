40ebbb3006982a6d78aa24aefa55f8ff134708a3
Jamie Farrelly <jamie.farrelly@paywithfire.com>
2016-12-26 22:34:38 +0000

    starting project, pom copied over from sample

---
+++ src/main/java/com/jamiefarrelly/PayWithFireAlexa/App.java
@@ -0,0 +1,13 @@
+package com.jamiefarrelly.PayWithFireAlexa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
